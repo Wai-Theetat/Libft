727ec8388647eff436cd461717be11b292ddd3f0
bLuEscReEn <theetat.ae@gmail.com>
2025-06-16 15:44:21 +0700

    ft_strstr v2

--- Libft/ft_strstr.c
+++ Libft/ft_strstr.c
@@ -23,12 +23,11 @@ char	*ft_strstr(const char *haystack, const char *needle)
 	while (haystack[i] != '\0')
 	{
 		j = 0;
-		while (haystack[i + j] != '\0' && haystack[i + j] == needle[j])
-		{
-			if (needle[j + 1] == '\0')
-				return ((char *)&haystack[i]);
+		while (haystack[i + j] != '\0' && needle[j] != '\0'
+			&& haystack[i + j] == needle[j])
 			j++;
-		}
+		if (needle[j] == '\0')
+			return ((char *)(haystack + i));
 		i++;
 	}
 	return (NULL);
