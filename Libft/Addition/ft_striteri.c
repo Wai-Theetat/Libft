5c356e0684a2d1aba63778bc2dfb6226076e3e39
bLuEscReEn <theetat.ae@gmail.com>
2025-06-19 13:47:42 +0700

    fix striteri.c

--- Libft/Addition/ft_striteri.c
+++ Libft/Addition/ft_striteri.c
@@ -6,7 +6,7 @@
 /*   By: tdharmar <tdharmar@student.42bangkok.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/19 13:40:25 by tdharmar          #+#    #+#             */
-/*   Updated: 2025/06/19 13:44:09 by tdharmar         ###   ########.fr       */
+/*   Updated: 2025/06/19 13:46:25 by tdharmar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,11 +17,11 @@ void	ft_striteri(char *s, void (*f)(unsigned int, char*))
 	unsigned int	i;
 
 	if (!s || !f)
-		return (NULL);
+		return ;
 	i = 0;
 	while (s[i])
 	{
-		f(i, s[i]);
+		f(i, &s[i]);
 		i++;
 	}
 }
