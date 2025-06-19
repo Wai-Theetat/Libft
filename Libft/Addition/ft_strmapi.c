6a69940d7a82a8076820cc1506f1bf686bcff899
bLuEscReEn <theetat.ae@gmail.com>
2025-06-19 13:19:42 +0700

    Update ft_strmapi.c

--- Libft/Addition/ft_strmapi.c
+++ Libft/Addition/ft_strmapi.c
@@ -6,7 +6,7 @@
 /*   By: tdharmar <tdharmar@student.42bangkok.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/19 12:01:39 by tdharmar          #+#    #+#             */
-/*   Updated: 2025/06/19 13:15:10 by tdharmar         ###   ########.fr       */
+/*   Updated: 2025/06/19 13:18:55 by tdharmar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,4 +20,14 @@ char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
 	
 	i = 0;
 	len = ft_strlen(s);
+	res = (char *)malloc(sizeof(char) * (len + 1));
+	if (!res)
+		return (NULL);
+	while (i  < len)
+	{
+		res[i] = f(i, s[i]);
+		i++;
+	}
+	res[i] = '\0';
+	return (res);
 }
